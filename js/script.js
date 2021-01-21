c67e2524725007fe7965e4920ba94eb55f87d667
Suriya KS <suriya2002.cse@gmail.com>
2021-01-21 17:45:02 +0530

    Implemented basic functionalities

--- js/script.js
+++ js/script.js
@@ -8,3 +8,28 @@ document.addEventListener('mousemove', event => {
         element.style.transform = `translate(calc(-50% - ${x}px), calc(-50% - ${y}px))`;
     });
 });
+
+const init = () => {
+    document.querySelectorAll('.form__input')[0].focus();
+
+    document.querySelectorAll('.icon-next').forEach(arrowNext => {
+        arrowNext.addEventListener('click', () => {
+            const currentParent = document.querySelector('.form__group--active');
+            const nextParent = currentParent.nextElementSibling;
+
+            const input = nextParent.querySelector('.form__input');
+            input.focus();
+
+            nextSlide(currentParent, nextParent);
+        });
+    });
+};
+
+const nextSlide = (currentParent, nextParent) => {
+    currentParent.classList.remove('form__group--active');
+    currentParent.classList.add('form__group--inactive');
+    nextParent.classList.remove('form__group--inactive');
+    nextParent.classList.add('form__group--active');
+};
+
+init();
